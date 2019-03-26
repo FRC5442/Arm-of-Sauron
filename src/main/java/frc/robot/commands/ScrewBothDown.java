b764d6b11be259e8471493511e5e15cfcd45d0a6
SJBealmer <sjbealmer@wacohi.net>
2019-03-26 17:37:01 -0500

    Competition changes

--- src/main/java/frc/robot/commands/ScrewBothDown.java
+++ src/main/java/frc/robot/commands/ScrewBothDown.java
@@ -43,7 +43,7 @@ public class ScrewBothDown extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return Math.abs(RobotMap.encoderScrewBack.getDistance()) > 21800.25;
+    return false;
   }
 
   // Called once after isFinished returns true
